a28527fa010c57ff900fe92ecba3d0bdf7f671bf
inspjosh150607 <150380172+inspjosh150607@users.noreply.github.com>
2025-08-19 14:27:12 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'Mide Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "Mide Bot",
+  botOwner: 'Mide', // Your name
+  ownerNumber: '2349023371564', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.0.9",
 };
